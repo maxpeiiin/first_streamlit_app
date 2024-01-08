c3205d052da9066af64fb3d9b3343557184f5dae
maxpeiiin <155702759+maxpeiiin@users.noreply.github.com>
2024-01-08 19:22:59 +0800

    Update streamlit_app.py
    lab

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,18 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_to_show = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_to_show]
 
+# # Allow the end user to add a fruit to the list
+# def insert_row_snowflake(new_fruit):
+# 	with my_cnx.cursor() as my_cur:
+# 		my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# 		return "Thanks for adding " + new_fruit
+
 # Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
 	with my_cnx.cursor() as my_cur:
-		my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+		my_cur.execute("insert into fruit_load_list values ('"+ new_fruit +"')")
 		return "Thanks for adding " + new_fruit
+		
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
