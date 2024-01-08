07b649644596e30295241742e9d8fecf820bc79f
maxpeiiin <155702759+maxpeiiin@users.noreply.github.com>
2024-01-08 19:06:18 +0800

    Update streamlit_app.py
    Move the Fruit Load List Query and Load into a Button Action pt3

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ try:
 except URLError as e:
 	streamlit.error()
 
-stremlit.header("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
 	with my_cnx.cursor() as my_cur:
